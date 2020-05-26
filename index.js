41027364a148258f6e72b1af7efe5e6bf1d74780
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-26 23:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const arr=["Ada", "Brendan", "Ali"];
+const birthday="or the wonderful birthday gift!";
+function writeCards(arr, birthday){
+  for(let i=0;i<arr.length;i++)
+  {
+    console.log("Thank you, arr[i], for the wonderful birthday gift!");
+  }
+  return(arr,birthday);
+}
+writeCards(arr,birthday);
+
+let countdown=0;
+while(countdown<=10){
+ console.log(countdown--);
+}
