b8972edb161fd3926b074adda458b373356e3ec2
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-26 23:51:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 const arr=["Ada", "Brendan", "Ali"];
-const birthday="or the wonderful birthday gift!";
+const birthday="for the wonderful birthday gift!";
 function writeCards(arr, birthday){
   for(let i=0;i<arr.length;i++)
   {
-    console.log("Thank you, arr[i], for the wonderful birthday gift!");
+    console.log("Thank you, arr[i], birthday[i]");
   }
   return(arr,birthday);
 }
