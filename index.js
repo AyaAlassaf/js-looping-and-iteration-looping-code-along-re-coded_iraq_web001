7a98624631f55b978c2c2c4dd9cf3bf11662962b
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-27 00:27:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,3 @@ while(integer >=0){
 }
 }
 countdown(10);
-function writeCards(names,eventName){
-for( let i=0;i<=names.length-1;i++){
-
-  names[i] =  `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
-}
-return names;
-}
-writeCards(["Lisa","Kaitlin","Jan"],"surprise");
